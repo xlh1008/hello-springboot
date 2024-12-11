6dc3ca40b5ada3918b335a3dda8f4c7b37b22250
xulonghui <17607977031@163.com>
2024-12-11 13:36:34 +0800

    push

---
+++ src/test/java/com/xlh/hellospringboot/HelloSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xlh.hellospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
